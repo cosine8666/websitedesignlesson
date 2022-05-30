9acd15fb90249c0a0f39675faa87ad7d79206549
cosine8666 <cosine1453476@gmail.com>
2022-05-30 12:19:40 +0800

    click事件完成

---
+++ source/mysite.js
@@ -0,0 +1,255 @@
+// 外部引入CSS2檔案
+// function dynamicLoadCss(url) {
+//     var head = document.getElementsByTagName('head')[0];
+//     var link = document.createElement('link');
+//     link.type='text/css';
+//     link.rel = 'stylesheet';
+//     link.src = "./mysite.css";
+//     head.appendChild(link);
+//     }
+//     dynamicLoadCss("./mysite.css");  //自动调用函数;
+function news1() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/news1.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function congratulate3() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/congratulate3.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function congratulate2() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/congratulate2.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function congratulate1() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/congratulate1.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function congratulate4() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/congratulate4.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function inviatation() {
+    let boxObj = document.getElementById("header");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/inviatation.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function lecture1() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/lecture1.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function lecture2() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/lecture2.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function lecture3() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/lecture3.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function lecture4() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/lecture4.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function lecture5() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/lecture5.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function activity1() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/activity1.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function activity2() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/activity2.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+
+function activity3() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/activity3.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+
+function activity4() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/activity4.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+
+function activity5() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/activity5.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration1() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration1.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration2() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration2.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration3() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration3.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration4() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration4.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration5() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration5.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration6() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration6.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration7() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration7.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration8() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration8.png";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function collaboration9() {
+    let boxObj = document.getElementById("activity");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/collaboration9.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function classprocess() {
+    let boxObj = document.getElementById("course");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/classprocess.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function coursestructure() {
+    let boxObj = document.getElementById("course");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/coursestructure.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+function career() {
+    let boxObj = document.getElementById("bookpage1");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/career.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+
+function scholar() {
+    let boxObj = document.getElementById("bookpage3");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/scholar.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
+
+function map() {
+    let boxObj = document.getElementById("map");//取得div物件
+    let imgObj = document.createElement("img");//建立圖片物件
+    imgObj.src = "source/map.jpg";
+    imgObj.width = "300";
+    boxObj.innerHTML = "";//清空div物件，刪除上一張圖片
+    boxObj.appendChild(imgObj);//將圖片加入div物件中
+}
